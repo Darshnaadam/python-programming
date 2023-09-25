0385dee05a1fb698a6edd81edb645065c5110835
Darshna <darshna.adam@gmail.com>
2023-09-25 15:08:36 +0530

    email bombing

---
+++ Email_bomber.py
@@ -0,0 +1,8 @@
+'''imports'''
+import smtplib # basically it is used to send emails on the network
+import sys 
+
+class bcolors:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED   = '\033[91m'
